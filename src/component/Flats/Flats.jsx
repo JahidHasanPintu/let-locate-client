3747fa31ad2132bda5833af0e530c86e012a1296
smfahim25 <smfahimhossen@gmail.com>
2023-05-26 09:20:51 +0600

    file added

---
+++ src/component/Flats/Flats.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Flats = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Flats;
---
+++ src/component/Roomates/Roomates.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Roomates = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Roomates;
